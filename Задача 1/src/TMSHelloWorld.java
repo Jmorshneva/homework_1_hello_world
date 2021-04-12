36c82ea6db84e47fce5c584836280055fb26a78a
Jmorshneva <51398847+Jmorshneva@users.noreply.github.com>
2021-04-12 20:35:25 +0300

    Added HelloWorld class

---
+++ Задача 1/src/TMSHelloWorld.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class TMSHelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.print("Введите ваше имя: ");
+        Scanner console = new Scanner (System.in);
+        String name = console.nextLine ();
+        System.out.println("Hello " + name);
+        System.out.println("Hello");
+        System.out.println("TeachMeSkills group");
+
+
+    }
+
+}
